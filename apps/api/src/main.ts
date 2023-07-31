84f155adf7042c89c27dea02987db2cab4d59f90
Emanuel Machado <emanuel@emachado.pt>
2023-07-31 20:10:24 +0100

    Did the whole login flow

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,13 +1,16 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 import User, { IUser } from './models/User';
 
 const host: string = process.env.HOST ?? 'localhost';
 const port: number = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-const loginDays = 7;
+// 7 days in milliseconds
+const loginTime = 7 * 24 * 60 * 60 * 1000;
 
 const generateOTP = (): string => {
   // Generate a 6-digit OTP
@@ -27,30 +30,102 @@ app.get('/', (_req: Request, res: Response) => {
   res.json({ message: 'Nothing to see here, move along' });
 });
 
+// TODO: DELETE THIS
+// Reset all users
+app.get('/reset', async (_req: Request, res: Response) => {
+  try {
+    await User.deleteMany({});
+    return res.json({ message: 'All users deleted' });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+});
+
+// Get all users
+app.get('/users', async (_req: Request, res: Response) => {
+  try {
+    const users = await User.find({});
+    return res.json({ users });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+});
+// END TODO
+
 // Login route
 app.post('/login', async (req: Request, res: Response) => {
-  const { email } = req.body;
+  const { email, password, otp } = req.body;
 
-  let user: IUser = await User.findOne({ email });
+  try {
+    let user: IUser = await User.findOne({ email });
 
-  if (!user) {
-    user = await User.create({ email });
+    // If the user doesn't exist, create it
+    if (!user) {
+      user = await User.create({ email });
+      return res.json({ requiresPassword: true });
+    }
+
+    // If the user is newly created, set the password and save
+    if (!user.password) {
+      user.password = await bcrypt.hash(password, 10);
+      await user.save();
+    }
+
+    if (!password) {
+      return res.json({ requiresPassword: true });
+    }
+    // Verify the password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: 'Invalid password' });
+    }
+
+    // If it's been more than X time since their last login, require OTP
+    if (
+      !otp &&
+      user.lastLogin !== null &&
+      Date.now() - user.lastLogin.getTime() > loginTime
+    ) {
+      const otp = generateOTP();
+      user.otp = otp;
+      await user.save();
+      return res.json({ requiresMfa: true });
+    }
+
+    // If there is OTP in the request, verify it
+    if (otp) {
+      if (otp !== user.otp) {
+        return res.status(401).json({ error: 'Invalid OTP' });
+      }
+      user.otp = undefined;
+      await user.save();
+    }
+
+    // Update the last login date
+    user.lastLogin = new Date();
+    await user.save();
+
+    const accessToken = jwt.sign(
+      { email: user.email },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: '7d',
+      }
+    );
+
+    // Return the access token
+    return res.json({ accessToken });
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal server error' });
   }
-
-  // If the user has never logged in before, or if it's been more than X days (7) since their last login, require MFA
-  if (!user.lastLogin || Date.now() - user.lastLogin.getTime() > loginDays * 24 * 60 * 60 * 1000) {
-    const otp = generateOTP();
-    return res.json({ mfaRequired: true, otp });
-  }
-
-  return res.send({ user });
 });
 
 // Init the API
 (async () => {
   // Connect to MongoDB
   await mongoose
-  .connect(process.env.MONGODB_URI)
+    .connect(process.env.MONGODB_URI)
     .then(() => console.log('[ ready ] Connected to DB'))
     .catch((err) => console.error('Error connecting to DB:', err));
 
@@ -59,4 +134,3 @@ app.post('/login', async (req: Request, res: Response) => {
     console.log(`[ ready ] http://${host}:${port}`);
   });
 })();
-
--- apps/ui/src/app/app.tsx
+++ apps/ui/src/app/app.tsx
@@ -7,8 +7,13 @@ const apiUrl = 'http://localhost:3000';
 
 export function App() {
   const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [passwordRequired, setPasswordRequired] = useState<boolean>(false);
+  const [mfaRequired, setMfaRequired] = useState<boolean>(false);
+  const [otp, setOtp] = useState<string>('');
+  const [accessToken, setAccessToken] = useState<string>(localStorage.getItem('accessToken') || '');
 
-  const handleLoginSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     // Test email address
@@ -18,31 +23,91 @@ export function App() {
     }
 
     try {
-      const { data } = await axios.post(`${apiUrl}/login`, { email });
-      console.log(data);
-    } catch (error) {
-      alert('Login failed!');
-      console.error(error);
+      const {
+        data: { accessToken, requiresMfa, requiresPassword },
+      } = await axios.post(`${apiUrl}/login`, { email, ...(password && { password }), otp });
+
+      setMfaRequired(requiresMfa);
+      setPasswordRequired(requiresPassword);
+
+      if (accessToken) {
+        localStorage.setItem('accessToken', accessToken);
+        setAccessToken(accessToken);
+      }
+    } catch (error: any) {
+      alert(error.response.data.error);
     }
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('accessToken');
+    setAccessToken('');
+    setEmail('');
+    setPassword('');
+    setOtp('');
   };
 
   return (
     <div>
-      <h1>XPTO Sys</h1>
+      <h1>XPTO Login Sys</h1>
       <section>
-        <h3>Login</h3>
-        <form onSubmit={handleLoginSubmit}>
-          <div className='mb-5'>
-            <label htmlFor="email">Email*</label>
-          </div>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)}
-          />
-          <button type="submit">Login</button>
-        </form>
+        {accessToken ? (
+          <>
+            <h3>Logged in</h3>
+            <button onClick={handleLogout}>Logout</button>
+          </>
+          ) : (
+          <>
+            <h3>Login</h3>
+            <form onSubmit={handleLogin}>
+              {!passwordRequired && !mfaRequired && (
+                <div className="mt-10">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                      setEmail(event.target.value)
+                    }
+                  />
+                </div>
+              )}
+
+              {passwordRequired && (
+                <div className="mt-10">
+                  <label htmlFor='password'>Password</label>
+                  <input
+                    id='password'
+                    type="password"
+                    value={password}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                      setPassword(event.target.value)
+                    }
+                    required
+                  />
+                </div>
+              )}
+
+              {mfaRequired && (
+                <div className="mt-10">
+                  <label htmlFor='otp'>One-Time Password (OTP)</label>
+                  <input
+                    id='otp'
+                    type="text"
+                    value={otp}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                      setOtp(event.target.value)
+                    }
+                    pattern="\d{6}"
+                    required
+                  />
+                </div>
+              )}
+              <button type="submit" className='mt-10'>Login</button>
+            </form>
+          </>
+        )}
       </section>
     </div>
   );
