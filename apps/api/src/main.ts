6115e6c8963394b1b94898baea4c814a31c4d6d2
Emanuel Machado <emanuel@emachado.pt>
2023-07-31 12:07:08 +0100

    [API] generate OTP

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,9 +7,18 @@ import User, { IUser } from './models/User';
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-const app: Application = express();
+const loginDays = 7;
 
-// Express middleware
+const generateOTP = (): string => {
+  // Generate a 6-digit OTP
+  const otp = Math.floor(100000 + Math.random() * 900000).toString();
+  // Log it per requirement
+  console.log('OTP:', otp);
+  return otp;
+};
+
+// Express and middleware
+const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
@@ -19,8 +28,8 @@ mongoose
   .then(() => console.log('[ ready ] Connected to DB'))
   .catch((err) => console.error('Error connecting to DB:', err));
 
-app.get('/', (req: Request, res: Response) => {
-  res.send({ message: 'Nothing to see here, move along' });
+app.get('/', (_req: Request, res: Response) => {
+  res.json({ message: 'Nothing to see here, move along' });
 });
 
 app.post('/login', async (req: Request, res: Response) => {
@@ -30,6 +39,12 @@ app.post('/login', async (req: Request, res: Response) => {
 
   if (!user) {
     user = await User.create({ email });
+  }
+
+  // If the user has never logged in before, or if it's been more than X days (7) since their last login, require MFA
+  if (!user.lastLogin || Date.now() - user.lastLogin.getTime() > loginDays * 24 * 60 * 60 * 1000) {
+    const otp = generateOTP();
+    return res.json({ mfaRequired: true, otp });
   }
 
   return res.send({ user });
