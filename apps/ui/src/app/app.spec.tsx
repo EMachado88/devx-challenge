4c95502a89ce49fe23a9c4aafc126f301ac57b30
Emanuel Machado <emanuel@emachado.pt>
2023-08-01 11:03:14 +0100

    [FE] Unit tests

--- apps/ui/src/app/app.spec.tsx
+++ apps/ui/src/app/app.spec.tsx
@@ -1,8 +1,13 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
+import jest from 'jest-mock';
 
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './app';
+
+window.alert = jest.fn();
+
+const email = 'mofo@yolo.com';
 
 describe('App', () => {
   it('should render successfully', () => {
@@ -20,6 +25,81 @@ describe('App', () => {
         <App />
       </BrowserRouter>
     );
-    expect(getByText(/Welcome ui/gi)).toBeTruthy();
+    expect(getByText(/XPTO Login Sys/gi)).toBeTruthy();
+  });
+
+  it('should have a login form', () => {
+    const { getByLabelText } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    expect(getByLabelText(/Email/gi)).toBeTruthy();
+  });
+
+  it('should have a login button', () => {
+    const { queryByRole } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    expect(queryByRole('button')?.getAttribute('type')).toBe('submit');
+  });
+
+  it('should show an alert if the email is invalid', async () => {
+    const { getByRole, getByLabelText } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+
+    const emailInput = getByLabelText(/Email/gi) as HTMLInputElement;
+    const loginButton = getByRole('button') as HTMLButtonElement;
+
+    emailInput.focus();
+    emailInput.blur();
+
+    loginButton.click();
+
+    expect(window.alert).toBeCalledTimes(1);
+  });
+
+  it('should render password input if the email is valid', async () => {
+    const { getByRole, getByLabelText } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+
+    const emailInput = getByLabelText(/Email/gi) as HTMLInputElement;
+    const loginButton = getByRole('button') as HTMLButtonElement;
+
+    emailInput.focus();
+    fireEvent.change(emailInput, { target: { value: email } });
+    fireEvent.click(loginButton);
+
+    const passwordInput = await waitFor(() => getByLabelText(/Password/gi))
+
+    expect(passwordInput).toBeTruthy();
+  });
+
+  it('should show an alert if the password is invalid', async () => {
+    const { getByRole, getByLabelText } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+
+    const emailInput = getByLabelText(/Email/gi) as HTMLInputElement;
+    const loginButton = getByRole('button') as HTMLButtonElement;
+
+    fireEvent.change(emailInput, { target: { value: email } });
+    fireEvent.click(loginButton);
+
+    const passwordInput = await waitFor(() => getByLabelText(/Password/gi))
+
+    fireEvent.change(passwordInput, { target: { value: '123' } });
+
+    expect(window.alert).toBeCalledTimes(1);
   });
 });
